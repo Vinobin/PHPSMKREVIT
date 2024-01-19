b6dae30fea3d35d7b0b4916fb534ce41afec5f88
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-01-19 20:13:59 +0700

    v 1.0
    menampilkan tulisan dan html

---
+++ smk.php
@@ -0,0 +1,10 @@
+<?php
+echo "belajar php";
+echo '<br>';
+echo 'saya siswa smk';
+echo '<h1 style="background-color:red">belajar php itu mudah</h1>
+<p style="background-color:blue">aku suka php</p>
+';
+echo 'saya belajar'.'php'.'<br>';
+echo 2019 + 2;
+?>
