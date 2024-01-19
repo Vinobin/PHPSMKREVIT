9aa20711dc04806b9cdf5a63abfa828a038b8136
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-01-19 20:20:24 +0700

    v 1.1
    variabel tipe data dan komentar

--- smk.php
+++ smk.php
@@ -1,10 +1,10 @@
 <?php
-echo "belajar php";
-echo '<br>';
-echo 'saya siswa smk';
-echo '<h1 style="background-color:red">belajar php itu mudah</h1>
-<p style="background-color:blue">aku suka php</p>
-';
-echo 'saya belajar'.'php'.'<br>';
-echo 2019 + 2;
+$tulisan = ' saya belajar php';
+echo $tulisan.'<br>';
+$angka ='2019';
+echo 'tahun'.$angka.'<br>';
+var_dump($tulisan);
+echo'<br>';
+var_dump((int)$angka+2);
+
 ?>
