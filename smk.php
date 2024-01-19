eeeb6f05d0500ae65432c5acf7b8a7dc366ddb68
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-01-19 21:49:27 +0700

    v 1.8
    foreach php

--- smk.php
+++ smk.php
@@ -1,31 +1,19 @@
 <?php
-$nama=array("joni", "tojo", "budi", "siti", 100,2.5);
+$nama=array("tojo", "budi", "siti", 100,);
 var_dump($nama);
 echo "<br>";
-echo $nama[5];
 echo "<br>";
-// for($i=0; $i< 6; $i++){
-//      echo $nama[$i]."<br>";
-// }
-// foreach ($nama as $k){
-//     echo $k.'<br>';
-// }
+foreach ($nama as $key){
+    echo $key.'<br>';
+}
 $nama=array(
-    "joni"=>"surabaya",
     "budi"=>"malang",
     "tojo"=>"jakarta",
     "siti"=>"sidoarjo"
 );
-$nama["joni"]="surabaya";
-$nama["budi"]="malang";
-$nama["tojo"]="jakarta";
-$nama["siti"]="sidoarjo";
-$nama["edi"]="semarang";
 var_dump($nama);
-echo"<br>";
-echo $nama['budi'];
 foreach ($nama as $key =>$value){
-    echo $key."=>".$value;
-    echo "<br>";
+    echo $key.'-'.$value;
+    echo '<br>';
 }
 ?>
