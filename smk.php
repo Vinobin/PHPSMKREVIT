f7b347482047826294cf5b91ab476843d1337fcf
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-01-20 09:59:06 +0700

    v 1.9
    require once php

--- smk.php
+++ smk.php
@@ -1,19 +1,3 @@
 <?php
-$nama=array("tojo", "budi", "siti", 100,);
-var_dump($nama);
-echo "<br>";
-echo "<br>";
-foreach ($nama as $key){
-    echo $key.'<br>';
-}
-$nama=array(
-    "budi"=>"malang",
-    "tojo"=>"jakarta",
-    "siti"=>"sidoarjo"
-);
-var_dump($nama);
-foreach ($nama as $key =>$value){
-    echo $key.'-'.$value;
-    echo '<br>';
-}
+require_once "nav.php";
 ?>
