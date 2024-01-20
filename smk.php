814e3c8999ca37e67e53d3e95d2a326e4c7cdc2f
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-01-20 12:03:59 +0700

    v 2.4
    upload file

--- smk.php
+++ smk.php
@@ -1,11 +1,19 @@
+<form action="" method="post" enctype="multipart/form-data">
+    pilih file gambar :
+    <input type="file" name="upload">
+    <input type="submit" name="kirim" value="simpan">
+</form>
+
 <?php
-$cookie_name='user';
-$cookie_value='joni';
-setcookie($cookie_name, $cookie_value);
-$cookie_value='tejo';
-setcookie($cookie_name, $cookie_value);
-echo $_COOKIE[$cookie_name];
-setcookie("user", "", time() - 3600);
-echo'<br>';
-var_dump($_COOKIE);
+   if(isset($_POST['kirim'])){
+    $file=$_FILES['upload'];
+    // var_dump($_FILES['upload']);
+    foreach ($file as $key => $value){
+        echo $key.'= '.$value;
+        echo '<br>';
+    }
+    $name=$_FILES['upload']['name'];
+    $temp=$_FILES['upload']['tmp_name'];
+    echo $name.'='.$temp;
+   }
 ?>
