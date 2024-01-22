c11bbe113ff7df1c450a58b9502fa0176a955e28
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-01-22 20:09:34 +0700

    v 3.2
    pembuatan layout aplikasi restoran berbasis web

---
+++ toko/admin/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>admin page | aplikasi toko smk</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+       <div class="row">
+        <div class="col-md-3">
+            <h3>toko</h3>
+        </div>
+        <div class="col-md-9">
+             <div class="float-right mt-4"></div>
+        </div>
+       </div>
+       <div class="row mt-5">
+        <div class="col-md-3">
+            <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="#">kategori</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">nama</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">pesan</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">pesan detail</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">user</a></li>
+            </ul>
+        </div>
+        <div class="col-md-9"></div>
+       </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p class="text-center">2024 - copyright@bintangrahmad.com</p>
+        </div>
+    </div>
+</body>
+</html>
