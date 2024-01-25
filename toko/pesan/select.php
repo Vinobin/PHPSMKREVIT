337f3996915fbc06624cc174115f07d66b183d1f
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-01-25 21:13:33 +0700

    v 3.7
    menampilkan gambar aplikasi restoran berbasis web

--- toko/pesan/select.php
+++ toko/pesan/select.php
@@ -25,7 +25,7 @@ $row=$db->getALL("SELECT * FROM barang ORDER BY kategori ASC");
   </form>
 </div>
 <?php
-$jumlahdata=$db->rowCOUNT("SELECT id FROM barang");
+$jumlahdata=$db->rowCOUNT("SELECT id FROM barang $where");
 $banyak=3;
 $halaman=$ceil($jumlahdata/$banyak);
   $sql="SELECT * FROM barang $where ORDER BY id ASC LIMIT $mulai,$banyak";
@@ -34,11 +34,13 @@ $halaman=$ceil($jumlahdata/$banyak);
   
 ?>
 
-<table class="table table-bordered w-50">
+<table class="table table-bordered w-80">
     <thead>
         <tr>
            
             <th>menu</th>
+            <th>harga</th>
+            <th>gambar</th>
             <th>delete</th>
             <th>update</th>
         </tr>
@@ -48,7 +50,9 @@ $halaman=$ceil($jumlahdata/$banyak);
         <?php foreach($row as $r): ?>
         <tr>
            
-            <td><?php echo $r['barang']?></td>
+            <td><?php echo $r['menu']?></td>
+            <td><?php echo $r['harga']?></td>
+            <td><img style="width:80px;" src="../upload/<?php echo $r['gambar']?>" alt=""></td>
             <td><a href="href="?f=menu&m=delete&id=<?php echo $r['id']?>">delete</td>
             <td><a href="href="?f=menu&m=update&id=<?php echo $r['id']?>">update</td>
         </tr>
