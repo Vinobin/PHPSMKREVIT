177816c65565147e34f60acd55d6675be47ac972
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-02-19 21:49:58 +0700

    v 6.1
    BACK END - pembuatan level akses

--- toko/admin/index.php
+++ toko/admin/index.php
@@ -24,21 +24,50 @@ session_start();
     <div class="container">
        <div class="row">
         <div class="col-md-3">
-            <h3>toko</h3>
+           <a href="index.php"> <h3>toko</h3></a>
         </div>
         <div class="col-md-9">
              <div class="float-right mt-4"><a href="?log=logout">logout</a></div>
              <div class="float-right mt-4 mr-4">user : <a href ="?f=user&m=updateuser&id"> <?php echo $_SESSION['iduser'] ?></a></div>
+             <div class="float-right mt-4" mr-4>LEVEL : <?php echo $_SESSION['level'] ?></div>
              <?php var_dump($_SESSION); ?>
         </div>
        </div>
        <div class="row mt-5">
         <div class="col-md-3">
             <ul class="nav flex-column">
-                <li class="nav-item"><a class="nav-link" href="?f=kategori&m=select">kategori</a></li>
-                <li class="nav-item"><a class="nav-link" href="?f=nama&m=select">nama</a></li>
-                <li class="nav-item"><a class="nav-link" href="?f=pesan&m=select">pesan</a></li>
-                <li class="nav-item"><a class="nav-link" href="?f=pelanggan&m=select">pelanggan</a></li>
+                <?php
+                $level=$_SESSION['level'];
+                switch ($level){
+                    case 'admin':
+                   echo '
+                    <li class="nav-item"><a class="nav-link" href="?f=kategori&m=select">kategori</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=menu&m=select">menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=pelanggan&m=select">pelanggan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=order&m=select">order</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=orderdetail&m=select">orderdetail</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=user&m=select">user</a></li>
+                   
+                    ';
+                    break;
+                    case 'kasir' :
+                          echo '
+                          <li class="nav-item"><a class="nav-link" href="?f=order&m=select">order</a></li>
+                          <li class="nav-item"><a class="nav-link" href="?f=orderdetail&m=select">orderdetail</a></li>
+                          ';
+                          break;
+                          echo 'koki' :
+                          echo '
+                          
+                          <li class="nav-item"><a class="nav-link" href="?f=orderdetail&m=select">orderdetail</a></li>
+                          ';
+                          break
+                    }
+                    default:
+                          echo 'tidak ada menu';
+                    break;
+                }
+                
             </ul>
         </div>
         <div class="col-md-9">
